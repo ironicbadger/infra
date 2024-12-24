c9f8ff38c52d2776cd95c820d9a9ad05f09f7ad5
Alex Kretzschmar <alexktz@gmail.com>
2024-12-23 21:57:37 -0500

    changes to name of secrets file

--- git-init.sh
+++ git-init.sh
@@ -6,11 +6,12 @@
 
 if [ -d .git/ ]; then
 rm .git/hooks/pre-commit
+mkdir -p .git/hooks/
 cat <<EOT >> .git/hooks/pre-commit
-if ( git show :vars/vault.yaml | grep -q "\$ANSIBLE_VAULT;" ); then
+if ( git show :vars/secrets.yaml | grep -q "\$ANSIBLE_VAULT;" ); then
 echo "[38;5;108mVault Encrypted. Safe to commit.[0m"
 else
-echo "[38;5;208mVault not encrypted! Run 'make encrypt' and try again.[0m"
+echo "[38;5;208mVault not encrypted! Run 'just vault encrypt' and try again.[0m"
 exit 1
 fi
 EOT
